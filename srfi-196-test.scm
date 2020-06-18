180ee5bfe5f3528a7a2139278ef44438b6b45948
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2020-06-18 09:55:27 -0400

    Remove non-portable tests for error conditions. Comment cleanup.

--- srfi-196-test.scm
+++ srfi-196-test.scm
@@ -1,29 +1,29 @@
-;; Copyright (C) 2020 Wolfgang Corcoran-Mathe
+;;; Copyright (C) 2020 Wolfgang Corcoran-Mathe
 
-;; Permission is hereby granted, free of charge, to any person obtaining a
-;; copy of this software and associated documentation files (the
-;; "Software"), to deal in the Software without restriction, including
-;; without limitation the rights to use, copy, modify, merge, publish,
-;; distribute, sublicense, and/or sell copies of the Software, and to
-;; permit persons to whom the Software is furnished to do so, subject to
-;; the following conditions:
+;;; Permission is hereby granted, free of charge, to any person obtaining a
+;;; copy of this software and associated documentation files (the
+;;; "Software"), to deal in the Software without restriction, including
+;;; without limitation the rights to use, copy, modify, merge, publish,
+;;; distribute, sublicense, and/or sell copies of the Software, and to
+;;; permit persons to whom the Software is furnished to do so, subject to
+;;; the following conditions:
 
-;; The above copyright notice and this permission notice shall be included
-;; in all copies or substantial portions of the Software.
+;;; The above copyright notice and this permission notice shall be included
+;;; in all copies or substantial portions of the Software.
 
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-;; OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-;; IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-;; CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+;;; OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;;; IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+;;; CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+;;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 (import (scheme base))
 (import (scheme write))
 (import (srfi 1))
 (import (srfi 128))
-(import (ranges))
+(import (srfi 196))
 
 (cond-expand
   ((library (srfi 78))
@@ -62,8 +62,8 @@
         (if (eof-object? v)
             '()
             (cons v (generator->list g))))))))
-
-;;; Utility
+
+;;;; Utility
 
 (define (identity x) x)
 
@@ -73,20 +73,10 @@
   (syntax-rules ()
     ((_ obj) (lambda _ obj))))
 
-;; Returns the value of `expr', or 'exception if an exception was raised.
-(define-syntax catch-exceptions
-  (syntax-rules ()
-   ((_ expr)
-    (call-with-current-continuation
-     (lambda (k)
-       (with-exception-handler
-        (lambda (_) (k 'exception))
-        (lambda () expr)))))))
-
 (define always (constantly #t))
 (define never (constantly #f))
 
-;;; Test ranges
+;;;; Test ranges
 
 (define test-num-range (numeric-range 10 30))
 
@@ -101,9 +91,9 @@
          2
          (lambda (b x) (if (zero? x) b (not b)))))
 
-;;; Conversion
+;;;; Conversion
 
-;;; Test these first, as range->list is used extensively in later tests.
+;;;; Test these first, as range->list is used extensively in later tests.
 
 (define (check-conversion)
   (check (range->list test-empty-range) => '())
@@ -113,21 +103,11 @@
                  (range->list test-num-range))
    => #t))
 
-;;; Constructors
-
 (define (check-constructors)
-  ;; range rejects ill-typed lower bounds.
-  (check (catch-exceptions
-           (range real-comparator 'z 100 (lambda (b n) (+ b n))))
-   => 'exception)
+  (check (range? (numeric-range 1 -5 -1))   => #t)
+  (check (range? (numeric-range 1.3 5.3 1)) => #t))
 
-  (check (range? (catch-exceptions (numeric-range 1 -5 -1)))   => #t)
-  (check (range? (catch-exceptions (numeric-range 1.3 5.3 1))) => #t)
-  (check (catch-exceptions (numeric-range 1 5.4))              => 'exception)
-  (check (catch-exceptions (numeric-range 1 -5 1))             => 'exception)
-  (check (catch-exceptions (numeric-range 1 5 0.3))            => 'exception))
-
-;;; Predicates
+;;;; Predicates
 
 (define (check-predicates)
   (check (range-contains? test-num-range (range-start test-num-range)) => #t)
@@ -146,19 +126,14 @@
 
   (check (range-empty? test-empty-range) => #t)
   (check (range-empty? test-num-range)   => #f))
-
-;;; Accessors
+
+;;;; Accessors
 
 (define (check-accessors)
   (check (range-ref test-num-range 0)  => 10)
-  (check (range-ref test-bool-range 1) => #t)
+  (check (range-ref test-bool-range 1) => #t))
 
-  (check (catch-exceptions (range-ref test-bool-range -1)) => 'exception)
-  (check (catch-exceptions (range-ref test-bool-range
-                                      (range-length test-bool-range)))
-   => 'exception))
-
-;;; Iteration
+;;;; Iteration
 
 (define (check-iteration)
   ;; Check lengths of ranges returned by range-split-at.
@@ -210,7 +185,7 @@
   (check (equal? (range-filter->list always test-bool-range)
                  (range->list test-bool-range))
    => #t)
-
+
   (check (null? (range-filter->list never test-bool-range)) => #t)
 
   ;; (range-filter->list pred r) = (filter pred (range->list r))
@@ -264,8 +239,8 @@
   (check (equal? (range->list (range-reverse test-num-range))
                  (reverse (range->list test-num-range)))
    => #t))
-
-;;; Searching
+
+;;;; Searching
 
 (define (check-searching)
   (check (range-index always test-num-range) => 0)
@@ -292,7 +267,7 @@
                  (range->list test-bool-range))
    => #t)
 
-  ;; Given a (non-existant) range-append function,
+  ;; Given a (non-existent) range-append function,
   ;;
   ;; (range-append (range-take-while p r) (range-drop-while p r)) = r
   (let ((pred (lambda (n) (< n 10))))
@@ -318,7 +293,7 @@
                  (range->list test-bool-range))
    => #t)
 
-  ;; Given a (non-existant) range-append function,
+  ;; Given a (non-existent) range-append function,
   ;;
   ;; (range-append (range-drop-while-right p r)
   ;;               (range-take-while-right p r)) = r
@@ -328,7 +303,7 @@
                     (range->list (range-take-while-right pred test-num-range)))
             (range->list test-num-range))
      => #t)))
-
+
 (define (check-all)
   (check-conversion)
   (check-constructors)
